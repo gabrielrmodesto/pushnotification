91ccb78791c5706a2f5edfaf5c69ee67b4861621
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-07 16:08:42 -0300

    Gerando o token do dispositivo

--- App.js
+++ App.js
@@ -1,45 +1,59 @@
+import React, {Component} from 'react';
 import { Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
+import {View,Text,StyleSheet} from 'react-native';
 
-const PUSH_ENDPOINT = 'https://colegasmedicos.com.br/api/medico/notificacao';
+class App extends Component{
+	registerForPushNotificationsAsync = async() => {
+	const { status: existingStatus } = await Permissions.getAsync(
+		Permissions.NOTIFICATIONS
+	);
+	let finalStatus = existingStatus;
 
-export default async function registerForPushNotificationsAsync() {
-  const { status: existingStatus } = await Permissions.getAsync(
-    Permissions.NOTIFICATIONS
-  );
-  let finalStatus = existingStatus;
+	// only ask if permissions have not already been determined, because
+	// iOS won't necessarily prompt the user a second time.
+	if (existingStatus !== 'granted') {
+		// Android remote notification permissions are granted during the app
+		// install, so this will only ask on iOS
+		const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+		finalStatus = status;
+	}
 
-  // only ask if permissions have not already been determined, because
-  // iOS won't necessarily prompt the user a second time.
-  if (existingStatus !== 'granted') {
-    // Android remote notification permissions are granted during the app
-    // install, so this will only ask on iOS
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    finalStatus = status;
-  }
+	// Stop here if the user did not grant permissions
+	if (finalStatus !== 'granted') {
+		return;
+	}
+	try {
+		// Get the token that uniquely identifies this device
+		let token = await Notifications.getExpoPushTokenAsync();
 
-  // Stop here if the user did not grant permissions
-  if (finalStatus !== 'granted') {
-    return;
-  }
+		// POST the token to your backend server from where you can retrieve it to send push notifications.
+		console.log(token);
 
-  // Get the token that uniquely identifies this device
-  let token = await Notifications.getExpoPushTokenAsync();
+	} catch (error) {
+		console.log(error);
+	}
+	
+	};
 
-  // POST the token to your backend server from where you can retrieve it to send push notifications.
-  return fetch(PUSH_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      token: {
-        value: token,
-      },
-      user: {
-        username: 'Brent',
-      },
-    }),
-  });
-}
+	async componentDidMount(){
+		await this.registerForPushNotificationsAsync();
+	}
+	render(){
+		return(
+			<View style={styles.container}>
+				<Text>Hello</Text>
+			</View>
+		)
+	}
+}	
+
+export default App;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
+});
